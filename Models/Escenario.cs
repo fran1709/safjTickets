d51b1f9a7ef878849bde536f10ccd1290dbbedc4
franj <fran1709@estudiantec.cr>
2023-03-22 15:41:00 -0600

    inital commit

---
+++ Models/Escenario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace samTicket.Models
+{
+    public partial class Escenario
+    {
+        public Escenario()
+        {
+            TipoEscenarios = new HashSet<TipoEscenario>();
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Localizacion { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public int UpdatedBy { get; set; }
+        public bool Active { get; set; }
+
+        public virtual ICollection<TipoEscenario> TipoEscenarios { get; set; }
+    }
+}
---
+++ Models/TipoEscenario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace samTicket.Models
+{
+    public partial class TipoEscenario
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public int UpdatedBy { get; set; }
+        public bool Active { get; set; }
+        public int IdEscenario { get; set; }
+
+        public virtual Escenario IdEscenarioNavigation { get; set; } = null!;
+    }
+}
